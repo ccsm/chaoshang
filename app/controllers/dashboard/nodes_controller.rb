d4f79e9083c466f708b8834f8ec0a4ede22394fb
suzhen <sz3001@gmail.com>
2013-08-22 11:28:37 +0800

    add form for tree

--- app/controllers/dashboard/nodes_controller.rb
+++ app/controllers/dashboard/nodes_controller.rb
@@ -2,6 +2,13 @@ class Dashboard::NodesController < Dashboard::DashboardController
   def index
     @sections = Section.all
     params[:active] ||= @sections.first.id.to_s
+    @node = Node.new
 
   end
+
+  def new
+   @node = Node.new
+
+  end
+
 end
